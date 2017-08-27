c90298c7335241e56bbd3cb852db8a177656e38d
Wongso <wswijaya@wongsos-mbp.home>
2017-08-27 13:53:32 +0800

    init

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var guessedNumber = 0
+    var randomNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        generateRandomNumber()
     }
 
+    @IBAction func pickNumber(_ sender: Any) {
+        //print((sender as! UIButton).currentTitle ?? "0")
+        guessedNumber = Int((sender as! UIButton).currentTitle ?? "0")!
+        
+        let difference = abs(randomNumber - guessedNumber)
+        let title: String
+        if (difference == 0) {
+            title = "Perfect!"
+        } else if (difference < 4) {
+            title = "You almost had it!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        let message = "Random number is \(randomNumber)\n and Your pick is \(guessedNumber)"
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let actionItem = UIAlertAction(title: "OK", style: .default) { [weak self]
+            action in
+                self?.generateRandomNumber()
+        }
+        alertController.addAction(actionItem)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func generateRandomNumber() {
+        randomNumber = Int(arc4random_uniform(10)) + 1
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
