8e02141bfe86110c25f44709ed88b99d1a4d020a
Wongso <wswijaya@wongsos-mbp.home>
2017-08-27 13:19:09 +0800

    Initial Commit

---
+++ GuessingGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessingGame
+//
+//  Created by Wongso on 27/8/17.
+//  Copyright © 2017 wswijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
